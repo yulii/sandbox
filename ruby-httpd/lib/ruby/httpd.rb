7dcc6fff0c0a8b114bbb00fb062bfe21eed4de60
Ryo Yoneyama (@yulii) <yone.info@gmail.com>
2016-05-17 12:07:30 +0900

    serve static file

--- ruby-httpd/lib/ruby/httpd.rb
+++ ruby-httpd/lib/ruby/httpd.rb
@@ -4,24 +4,74 @@ require 'socket'
 
 module Ruby
   module Httpd
+    require 'socket'
+    require 'uri'
+
+    WEB_ROOT = './public'
+
+    CONTENT_TYPE_MAPPING = {
+      'html' => 'text/html',
+      'txt' => 'text/plain',
+      'png' => 'image/png',
+      'jpg' => 'image/jpeg'
+    }
+
+    DEFAULT_CONTENT_TYPE = 'application/octet-stream'
+
+    def self.content_type(path)
+      ext = File.extname(path).split(".").last
+      CONTENT_TYPE_MAPPING.fetch(ext, DEFAULT_CONTENT_TYPE)
+    end
+
+    def self.requested_file(request_line)
+      request_uri  = request_line.split(" ")[1]
+      path         = URI.unescape(URI(request_uri).path)
+
+      clean = []
+
+      parts = path.split("/")
+      parts.each do |part|
+        next if part.empty? || part == '.'
+        part == '..' ? clean.pop : clean << part
+      end
+
+      File.join(WEB_ROOT, *clean)
+    end
+
     def self.run
       server = TCPServer.new('localhost', 2345)
 
       loop do
-        socket  = server.accept
-        request = socket.gets
+        socket       = server.accept
+        request_line = socket.gets
 
-        STDERR.puts request
+        STDERR.puts request_line
 
-        response = "Hello World!\n"
+        path = requested_file(request_line)
 
-        socket.print "HTTP/1.1 200 OK\r\n" +
-                     "Content-Type: text/plain\r\n" +
-                     "Content-Length: #{response.bytesize}\r\n" +
-                     "Connection: close\r\n"
-        socket.print "\r\n"
+        puts path
 
-        socket.print response
+        if File.exist?(path) && !File.directory?(path)
+          File.open(path, "rb") do |file|
+            socket.print "HTTP/1.1 200 OK\r\n" +
+                         "Content-Type: #{content_type(file)}\r\n" +
+                         "Content-Length: #{file.size}\r\n" +
+                         "Connection: close\r\n"
+            socket.print "\r\n"
+
+            IO.copy_stream(file, socket)
+          end
+        else
+          message = "File not found\n"
+
+          socket.print "HTTP/1.1 404 Not Found\r\n" +
+                       "Content-Type: text/plain\r\n" +
+                       "Content-Length: #{message.size}\r\n" +
+                       "Connection: close\r\n"
+          socket.print "\r\n"
+
+          socket.print message
+        end
 
         socket.close
       end
